37cd8871262b2b928ec645ab49407063a87d847d
Rupert Chen <rupert.chen@liveramp.com>
2018-05-17 20:35:43 -0400

    wip, finish Parse2
    Read* methods remain

--- parse.go
+++ parse.go
@@ -4,6 +4,7 @@ import (
 	"github.com/rupertchen/go-bits"
 	"time"
 	"encoding/base64"
+	"fmt"
 )
 
 type ConsentReader struct {
@@ -33,9 +34,33 @@ func (r *ConsentReader) ReadBoolMap(n uint) map[int]bool {
 	return make(map[int]bool)
 }
 
-func Parse2(s string) (*ParsedConsent, error) {
+// TODO: export rangeEntry or unexport this func
+func (r *ConsentReader) ReadRangeEntries(n uint) []*rangeEntry {
+	var ret = make([]*rangeEntry, 0, n)
+	for i := uint(0); i < n; i++ {
+		var isRange = r.ReadBool()
+		var start, end int
+		start = r.ReadInt(16)
+		if isRange {
+			end = r.ReadInt(16)
+		} else {
+			end = start
+		}
+		ret = append(ret, &rangeEntry{StartVendorID: start, EndVendorID: end})
+	}
+	return ret
+}
 
-	var b, err = base64.RawURLEncoding.DecodeString(s)
+func Parse2(s string) (p *ParsedConsent, err error) {
+	// This func leverages named returns to return partially parsed content when there is an error
+
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%v", r)
+		}
+	}()
+
+	b, err := base64.RawURLEncoding.DecodeString(s)
 	if err != nil {
 		return nil, err
 	}
@@ -43,8 +68,8 @@ func Parse2(s string) (*ParsedConsent, error) {
 	var r = NewConsentReader(b)
 
 	// This block of code directly describes the format of the payload.
-	var p = &ParsedConsent{}
-	// Is setting consentString, still interesting?
+	p = &ParsedConsent{}
+	// TODO: Is setting consentString, still interesting?
 	p.version = r.ReadInt(6)
 	p.created = r.ReadTime()
 	p.lastUpdated = r.ReadTime()
@@ -54,6 +79,16 @@ func Parse2(s string) (*ParsedConsent, error) {
 	p.consentLanguage = r.ReadString(2)
 	p.vendorListVersion = r.ReadInt(12)
 	p.purposesAllowed = r.ReadBoolMap(24)
+	p.maxVendorID = r.ReadInt(16)
+
+	var hasRanges = r.ReadBool()
+	if hasRanges {
+		p.defaultConsent = r.ReadBool()
+		p.numEntries = r.ReadInt(12)
+		p.rangeEntries = r.ReadRangeEntries(uint(p.numEntries))
+	} else {
+		p.approvedVendorIDs = r.ReadBoolMap(uint(p.maxVendorID))
+	}
 
 	return p, nil
 }
